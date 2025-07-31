52ea7a44ad224c4872ab6070598c1dfe7ceb8749
coding-demon-panda <adarshdhakar277@gmail.com>
2025-08-01 03:46:26 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import 'flowbite';
-import React from 'react';
-import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import ImportantLinks from './pages/ImportantLinks';
-import Gallery from './pages/Gallery';
-import Member from './pages/Member';
-import About from './pages/AboutUs';
-import ContactUs from './pages/ContactUs';
-import Notifications from './pages/Notifications';
-import RForm from './pages/RForm';
-import Admin from './pages/Admin';
+import RegexToDfaConverter from "./pages/RegexToDfaConverter"; 
 
-import Header from './components/Header'; // Import Header component
-import Footer from './components/Footer'; // Import Header component
-
-import { Provider } from 'react-redux';
-import { store } from './redux/store'; // Your store file
-
-const App = () => {
+function App() {
   return (
-    <Provider store={store}>  {/* Redux provider to give access to the store */}
-    <Router>
-      <Header /> {/* Include Header component */}
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/importantlinks" element={<ImportantLinks />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/member" element={<Member />} />
-        <Route path="/aboutus" element={<About />} />
-        <Route path="/contactus" element={<ContactUs />} />
-        <Route path="/notification" element={<Notifications />} />
-        <Route path="/salary" element={<RForm />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<RegexToDfaConverter />} />
       </Routes>
-      <Footer />
-    </Router>
-    </Provider>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
-
